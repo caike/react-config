5271674ce3f69e0d0b4ab8c9c54e9415e4f60044
Carlos Souza <carloshrsouza@gmail.com>
2015-05-07 11:29:39 -0400

    First Exercise

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 import React from 'react'
 
-class Component {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      hasProfile: false
+    };
+  }
+
+  onSubmitHandler(e){
+    e.preventDefault();
+    var firstNameNode = this.refs.firstName.getDOMNode();
+    var firstName = firstNameNode.value;
+
+    var lastNameNode = this.refs.lastName.getDOMNode();
+    var lastName = lastNameNode.value;
+
+    this.showProfile(firstName, lastName);
+  }
+
+  componentDidMount(){
+    this.refs.firstName.getDOMNode().focus();
+  }
+
+  showProfile(first, last){
+    var profile = `First Name: ${first}, Last Name: ${last}`;
+    this.setState({ hasProfile: true, profile: profile });
+  }
+
   render() {
-    return (
-      <h1>I'm ready to start me some Reactin'</h1>
-    )
+    var body;
+
+    if(this.state.hasProfile){
+      body = this.state.profile;
+    }else{
+      body = (<form onSubmit={this.onSubmitHandler.bind(this)}>
+        <p>
+          <input ref="firstName" placeholder="First Name" />
+        </p>
+        <p>
+          <input ref="lastName" placeholder="Last Name" />
+        </p>
+        <p>
+          <input type="submit" />
+        </p>
+      </form>);
+    }
+    return (<div> {body} </div>);
   }
 }
 
-React.render(<Component />, document.getElementById('app'))
+React.render(<App />, document.getElementById('app'))
